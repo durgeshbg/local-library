fec8838fcf9057fffa940b7c2be4572b5041e7a5
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-05-28 23:13:41 +0530

    add: compression module

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
 
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
@@ -27,6 +28,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
